6fc342d204a38508fa390cb75ef40b4323eb9da5
Christian Parker <christianparker512@gmail.com>
2021-06-01 21:50:43 -0500

    elif

--- blocks.py
+++ blocks.py
@@ -9,6 +9,8 @@ else:
 
 if age < 18:
     print("Please come back in {0} years".format(18-age))
+elif age ==900:
+    print("Sorry, Yoda, you die in the Return of the Jedi")
 else:
     print("You are old enough to vote")
     print("Please put an X in the box")
