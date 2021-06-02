7c8a40f4538819e348aaf9f02201a623be5ed74a
Christian Parker <christianparker512@gmail.com>
2021-06-01 21:47:12 -0500

    if else statements

--- blocks.py
+++ blocks.py
@@ -4,3 +4,11 @@ print(age)
 
 if age >= 18:
     print("You are od enough to vote")
+else:
+    print("Please come back in {0} years".format(18 -age))
+
+if age < 18:
+    print("Please come back in {0} years".format(18-age))
+else:
+    print("You are old enough to vote")
+    print("Please put an X in the box")
