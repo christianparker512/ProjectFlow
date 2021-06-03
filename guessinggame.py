0f1c83f51c0a2cad2f0c35ea27f37f84455e32a0
Christian Parker <christianparker512@gmail.com>
2021-06-03 10:04:55 -0500

    guessing game including if else with bang

--- guessinggame.py
+++ guessinggame.py
@@ -2,18 +2,30 @@ answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else: print("Sorry, you have not guessed correctly")
-elif guess > answer:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else: print("Sorry, you have not guessed correctly")
+#if guess < answer:
+    #print("Please guess higher")
+    #guess = int(input())
 
-else:
-    print("You chose wisely")
+
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guess it")
+    else:
+        print("Sorry, you have not guessed correctly")
+   # if guess == answer:
+       # print("Well done, you guessed it")
+    #else: print("Sorry, you have not guessed correctly")
+#elif guess > answer:
+    #print("Please guess lower")
+    #guess = int(input())
+    #if guess == answer:
+    #    print("Well done, you guessed it")
+   # else: print("Sorry, you have not guessed correctly")
+
+#else:
+   # print("You chose wisely")
