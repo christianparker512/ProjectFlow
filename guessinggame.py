629e02c5b491a053e45b7517babf0fa44931ee75
Christian Parker <christianparker512@gmail.com>
2021-06-03 09:51:35 -0500

    second guess

--- guessinggame.py
+++ guessinggame.py
@@ -1,3 +1,16 @@
 answer = 5
 print("Please guess number between 1 and 10: ")
 guess = int(input())
+
+if guess < answer:
+    print("Please guess higher")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+elif guess > answer:
+    print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+else:
+    print("You chose wisely")
