5c01d998f5ca3319ff7a276958de9894e09fc01a
Christian Parker <christianparker512@gmail.com>
2021-06-03 12:01:24 -0500

    printing characters on different lines

---
+++ ifchallenge.py
@@ -0,0 +1,7 @@
+name=input("Please enter your name: ")
+age = int(input("How old are you?"))
+
+if 50<= age <75:
+    print("welcome to the retired holidays, {0}".format(name))
+else:
+    print("I'm sorry, our holidays are only for the cool people")
