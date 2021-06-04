c8164246f952737ffce75230bb742385e13726ee
Christian Parker <christianparker512@gmail.com>
2021-06-04 11:22:44 -0500

    initialize and not found

--- searching.py
+++ searching.py
@@ -1,6 +1,6 @@
 shopping_list = ["apples", "grapefruit", "coffee", "brisket", "tortillas", "peppers", "onions"]
 
-item_to_find ="spam"
+item_to_find ="albatross"
 found_at = None
 
 # for index in range(6):
@@ -8,5 +8,7 @@ for index in range(len(shopping_list)):
     if shopping_list[index] == item_to_find:
         found_at = index
         break
-
-print("Item found at position{}".format(found_at))
+if found_at is not None:
+    print("Item found at position{}".format(found_at))
+else:
+    print("{} not found".format(item_to_find))
