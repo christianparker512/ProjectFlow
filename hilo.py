0deb06a9c85b558cf2b24580ab3633e48610801a
Christian Parker <christianparker512@gmail.com>
2021-06-04 15:17:13 -0500

    high low preliminary code

--- hilo.py
+++ hilo.py
@@ -1,2 +1,19 @@
 low = 1
 high = 1000
+
+print("Please think of a number between {} and {}".format(low,high))
+input("Press Enter to start")
+
+guess = 1
+while True:
+    guess = low + (high - low)//2
+    high_low = input("My guess is {}. Should I guess higher or lower? "
+                    "Enter h or l or c if the guess was correct"
+                    .format(guess)).casefold()
+    if high_low =="h":
+        #Guess higher: the low end of the range becomes 1 greater than the guess
+    elif high_low == "l":
+        # guess lower. the high end of hte range becomes one less than the guess.
+    elif high_low == "c":
+            print("I got it in {} guesses!".format(guess))
+    else: print("Please enter h, l or c")
