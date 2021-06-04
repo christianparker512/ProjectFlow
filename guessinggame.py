3eebeb643da5a61b777e7a6cf315ca9d061f6acf
Christian Parker <christianparker512@gmail.com>
2021-06-04 15:06:02 -0500

    binary search

--- guessinggame.py
+++ guessinggame.py
@@ -1,11 +1,8 @@
-answer = 5
+import random
+answer = random.randint(1,10)
+
 print("Please guess number between 1 and 10: ")
 guess = int(input())
-
-#if guess < answer:
-    #print("Please guess higher")
-    #guess = int(input())
-
 
 if guess != answer:
     if guess < answer:
@@ -17,15 +14,3 @@ if guess != answer:
         print("Well done, you guess it")
     else:
         print("Sorry, you have not guessed correctly")
-   # if guess == answer:
-       # print("Well done, you guessed it")
-    #else: print("Sorry, you have not guessed correctly")
-#elif guess > answer:
-    #print("Please guess lower")
-    #guess = int(input())
-    #if guess == answer:
-    #    print("Well done, you guessed it")
-   # else: print("Sorry, you have not guessed correctly")
-
-#else:
-   # print("You chose wisely")
