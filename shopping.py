5a54159ee5585abb14e5e9a2e43d655518533af0
Christian Parker <christianparker512@gmail.com>
2021-06-04 10:58:53 -0500

    the use of continue

--- shopping.py
+++ shopping.py
@@ -1,4 +1,11 @@
 shopping_list = ["apples", "grapefruit", "coffee", "brisket", "tortillas", "peppers", "onions"]
 
+#for item in shopping_list:
+ #   if item != "onions":
+  #      print("Buy " + item)
+
 for item in shopping_list:
+    if item == "onions":
+        continue
+
     print("Buy " + item)
